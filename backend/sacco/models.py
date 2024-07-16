3fd435dc5e7f6cebb4c6683cd1749669c8f2c547
Felix <93880031+felixkamau@users.noreply.github.com>
2024-07-16 21:25:44 +0000

    admin test

--- backend/sacco/models.py
+++ backend/sacco/models.py
@@ -37,7 +37,7 @@ class LoanApplication(models.Model):
 # Transaction modsel
 class Transaction(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    amount =  amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=50, choices=[
         ('Deposit', 'Deposit'),
         ('Withdrawal', 'Withdrawal'),
--- backend/sacco_api/admin.py
+++ backend/sacco_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from sacco.models import LoanApplication, Member, Transaction
 # Register your models here.
+admin.site.register(LoanApplication)
+admin.site.register(Member)
+admin.site.register(Transaction)
