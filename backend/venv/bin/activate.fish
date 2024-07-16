57395d65d59c0b34e8c2693f2558ca84cfc671f5
Felix <93880031+felixkamau@users.noreply.github.com>
2024-07-16 18:34:14 +0000

    initial commit

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/eugene/Desktop/test-lab/venv"
+set -gx VIRTUAL_ENV "/workspaces/sacco/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
