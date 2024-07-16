e96d06086a75e9591c3038c27e6180a3ce3c2443
Felix <93880031+felixkamau@users.noreply.github.com>
2024-07-16 18:58:01 +0000

    core settings and urls config

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('sacco.urls'), name='sacco'),
+    path('api/', include('api.urls'),name='sacco_api'),
 ]
